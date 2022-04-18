13569ad90c3bde16e5795537cf296751015a490d
2019211001001202-zengjiayi <1668026348@qq.com>
2022-04-19 01:50:30 +0800

    lab

--- LabAss2/Form1.cs
+++ LabAss2/Form1.cs
@@ -19,11 +19,31 @@ namespace LabAss2
 
         private void textBox6_TextChanged(object sender, EventArgs e)
         {
-            double n1=Convert .ToDouble(textNumber1.Text);
-            double n2=Convert .ToDouble(textNumber2.Text);
+            double n1 = Convert.ToDouble(textNumber1.Text);
+            double n2 = Convert.ToDouble(textNumber2.Text);
             double result = n1 + n2;
-            MessageBox.Show(n1 + "+"+ n2 + " = " + result);
-
+            MessageBox.Show(n1 + "+" + n2 + " = " + result);
+        }
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble(textNumber1.Text);
+            double n2 = Convert.ToDouble(textNumber2.Text);
+            double result = n1 - n2;
+            MessageBox.Show(n1 + "-" + n2 + " = " + result);
+        }
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        { 
+        double n1 = Convert.ToDouble(textNumber1.Text);
+        double n2 = Convert.ToDouble(textNumber2.Text);
+        double result = n1 / n2;
+        MessageBox.Show(n1 + "/" + n2 + " = " + result);
+            }
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        { 
+            double n1 = Convert.ToDouble(textNumber1.Text);
+            double n2 = Convert.ToDouble(textNumber2.Text);
+            double result = n1 * n2;
+            MessageBox.Show(n1 + "*" + n2 + " = " + result);
         }
     }
 }
